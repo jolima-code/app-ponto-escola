5f0679ae7182ceeec981da9541f1356f2ebb818a
Jo de Farias Lima <jo.lima@embrapa.br>
2026-05-18 00:34:59 -0300

    Adiciona rota de autocadastro

--- config/urls.py
+++ config/urls.py
@@ -9,21 +9,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path(
-        "login/",
-        auth_views.LoginView.as_view(
-            template_name="login.html"
-        ),
-        name="login"
-    ),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
 
-    path(
-        "logout/",
-        auth_views.LogoutView.as_view(
-            next_page="/login/"
-        ),
-        name="logout"
-    ),
+    path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
 
     path("", include("ponto.urls")),
 
