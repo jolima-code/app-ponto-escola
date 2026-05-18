7758e1b13deed91eeb3babcccbe31c5ad7883e81
Jo de Farias Lima <jo.lima@embrapa.br>
2026-05-17 23:13:42 -0300

    Cria superusuario automatico no deploy

--- build.sh
+++ build.sh
@@ -4,4 +4,18 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
 python manage.py migrate
+
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        "admin",
+        "jo.lima@embrapa.br",
+        "Admin123456!"
+    )
+END
