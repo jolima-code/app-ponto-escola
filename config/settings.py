70ea4d787b281ef3919dd68ad3f7e918fd528bba
Jo de Farias Lima <jo.lima@embrapa.br>
2026-05-21 00:17:49 -0300

    Define tema claro do admin

--- config/settings.py
+++ config/settings.py
@@ -36,16 +36,19 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'funcionarios',
-    'ponto',
-    'documentos',
-    'banco_horas',
+    "jazzmin",
+
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    "funcionarios",
+    "ponto",
+    "documentos",
+    "banco_horas",
 ]
 
 MIDDLEWARE = [
@@ -113,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -143,3 +146,61 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if DATABASE_URL:
     DATABASES["default"] = dj_database_url.parse(DATABASE_URL)
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Escola Cultivar",
+    "site_header": "Escola Cultivar",
+    "site_brand": "Escola Cultivar",
+    "welcome_sign": "Bem-vindo à Administração",
+    "copyright": "Escola Cultivar",
+
+    "site_logo": "img/logo.png",
+    "login_logo": "img/logo.png",
+    "login_logo_dark": "img/logo.png",
+
+    "show_sidebar": True,
+    "navigation_expanded": True,
+
+    "topmenu_links": [
+        {
+            "name": "Ver sistema",
+            "url": "/",
+        },
+    ],
+
+    "order_with_respect_to": [
+        "funcionarios",
+        "ponto",
+        "banco_horas",
+        "documentos",
+        "auth",
+    ],
+
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "funcionarios.Funcionario": "fas fa-id-badge",
+        "ponto.RegistroPonto": "fas fa-clock",
+        "documentos.Documento": "fas fa-file-alt",
+        "banco_horas.SaldoBancoHoras": "fas fa-hourglass-half",
+        "banco_horas.CompensacaoBancoHoras": "fas fa-exchange-alt",
+        "banco_horas.FechamentoBancoHoras": "fas fa-lock",
+    },
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "theme": "flatly",
+    "navbar": "navbar-primary navbar-dark",
+    "sidebar": "sidebar-light-primary",
+    "brand_colour": "navbar-primary",
+    "accent": "accent-primary",
+    "button_classes": {
+        "primary": "btn btn-primary rounded-pill",
+        "secondary": "btn btn-secondary rounded-pill",
+        "success": "btn btn-success rounded-pill",
+        "danger": "btn btn-danger rounded-pill",
+        "warning": "btn btn-warning rounded-pill",
+        "info": "btn btn-info rounded-pill",
+    },
+}
